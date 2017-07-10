eab01c7586437193d565789ed936c3bb54d5c0a9
KELLGGNCPU0238\somasekhar.reddy <pesala.somasekharreddy@kelltontech.com>
2017-07-10 15:51:03 +0530

    first commit

---
+++ src/main/java/com/tests4geeks/tutorials/MongoTutorialApplication.java
@@ -0,0 +1,17 @@
+package com.tests4geeks.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories("com.tests4geeks.tutorials.repository")
+public class MongoTutorialApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MongoTutorialApplication.class, args);
+	}
+	
+	
+}
